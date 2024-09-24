7b01073d5c3d3293968b621b4e690fef94c18f4f
Yiyuan LI <yy16@Mac16.local>
2024-09-24 22:00:41 +0800

    Deleted old step1_sequences_and_evolutionary_rate_estimation folder

--- step1_evolutionary_rate_estimation/set1_mt_genes/batch_aln.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for file in $(cat gene.list)
-do 
- echo ${file}.noStopCodon.fasta
- mafft-linsi ${file}.noStopCodon.fasta > ${file}.fasta.aln
- Gblocks ${file}.fasta.aln -t=p -b4=5 -b5=a -e=-gb
- less -S ${file}.fasta.aln-gb| perl -e '$/ = ">"; <>; while(<>){chomp; @line=split/\n+/; my $name = shift @line; @name=split/\s+/, $name; my $seq = join "",@line; $seq =~s/\s+//g; print ">$name[0]\n$seq\n";}' > ${file}.fasta.GBlocks
-done

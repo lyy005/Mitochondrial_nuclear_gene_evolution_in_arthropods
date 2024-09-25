a925a35b911b286f66fdbf4eaa534841b371bbd6
YY <lyy0005@gmail.com>
2024-09-25 08:16:14 +0800

    Delete step1_sequences_and_evolutionary_rate_estimation directory

--- step1_sequences_and_evolutionary_rate_estimation/set7_nucControlSingle/batch_aln.sh
+++
@@ -1,9 +0,0 @@
-for file in $(cat gene.list)
-do
- echo ${file}.fa
- perl pick_aa_seq_v0.9.pl ../aaSeq/${file}.fa ${file} nucControlSingle
- perl pick_aa_seq_versionRandom.pl nucControlSingle.${file}.fasta nucControlSingle.${file}.fasta.random
- mafft-linsi nucControlSingle.${file}.fasta.random > nucControlSingle.${file}.fasta.random.aln
- Gblocks nucControlSingle.${file}.fasta.random.aln -t=p -b4=5 -b5=a -e=-gb
- less -S nucControlSingle.${file}.fasta.random.aln-gb| perl -e '$/ = ">"; <>; while(<>){chomp; @line=split/\n+/; my $name = shift @line; @name=split/\s+/, $name; my $seq = join "",@line; $seq =~s/\s+//g; print ">$name[0]\n$seq\n";}' > nucControlSingle.${file}.fasta.random.GBlocks.fas
-done
--- step1_sequences_and_evolutionary_rate_estimation/set8_nucControl/batch_aln.sh
+++
@@ -1,9 +0,0 @@
-for file in $(cat gene.list)
-do
- echo ${file}.fa
- perl pick_aa_seq_v0.9.pl ../aaSeq/${file}.fa ${file} nucControl
- perl pick_aa_seq_versionRandom.pl nucControl.${file}.fasta nucControl.${file}.fasta.random
- mafft-linsi nucControl.${file}.fasta.random > nucControl.${file}.fasta.random.aln
- Gblocks nucControl.${file}.fasta.random.aln -t=p -b4=5 -b5=a -e=-gb
- less -S nucControl.${file}.fasta.random.aln-gb| perl -e '$/ = ">"; <>; while(<>){chomp; @line=split/\n+/; my $name = shift @line; @name=split/\s+/, $name; my $seq = join "",@line; $seq =~s/\s+//g; print ">$name[0]\n$seq\n";}' > nucControl.${file}.fasta.random.GBlocks.fas
-done
